294fdfade978101349d28348786fc481140c3b58
albedon <127307786+albedon@users.noreply.github.com>
2024-11-23 08:48:57 +0000

    fix bug

--- backend/site_settings/settings.py
+++ backend/site_settings/settings.py
@@ -98,7 +98,6 @@ WSGI_APPLICATION = "site_settings.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
         default=os.environ.get("DB_URL"),
         conn_max_age=600
     )
